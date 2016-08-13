739f750ba8c862e95b87569377cc876c30f70536
Ilia Dobrusin <iliadobrusin@freenet.de>
2016-08-13 16:15:37 +0200

    Converts RocDemoController to roc ROS node

---
+++ RocDemo/roc/scripts/models/Duration.py
@@ -0,0 +1,5 @@
+class Duration:
+    def __init__(self, duration=0, duration_unit=None, offset=0):
+        self.duration = duration
+        self.duration_unit = duration_unit
+        self.offset = offset
