c4e48d21880489c4fcc761ac1ce3c025aedb66e8
Ilia Dobrusin <iliadobrusin@freenet.de>
2016-08-14 12:39:49 +0200

    Delete legacy files.

--- RocDemo/roc/scripts/models/ActionUnit.py
+++
@@ -1,4 +0,0 @@
-class ActionUnit:
-    def __init__(self, action_unit=None, intesity=None):
-        self.number = action_unit
-        self.intensity = intesity
--- RocDemo/roc/scripts/models/Motion.py
+++
@@ -1,4 +0,0 @@
-class Motion:
-    def __init__(self, action_unit=None, duration=None):
-        self.action_unit = action_unit
-        self.duration = duration
